f7a882aa4ea81e3d6d4f17dfb98577e77a46a899
Kristina <k.nejgum.kn@gmail.com>
2022-07-08 10:48:34 +0200

    Deklaration+Wertzuweisung 2 und Konstanten

--- script_01.js
+++ script_01.js
@@ -14,10 +14,37 @@
 
 /***** 02  Deklaration + Wertzuweisung 1 *******/
 
-let firstName; // Deklaration (Definition)
-firstName = "Max";  // Wertzuweisung
-console.log(firstName);  // Ausgabe
+// let firstName; // Deklaration (Definition)
+// firstName = "Max";  // Wertzuweisung
+// console.log(firstName);  // Ausgabe
 
-let familyName = "Mütze";  // Deklaration + Wertzuweisung
-console.log(familyName);  // Ausgabe
-console.log(firstName + " " + familyName);
+// let familyName = "Mütze";  // Deklaration + Wertzuweisung
+// console.log(familyName);  // Ausgabe
+// console.log(firstName + " " + familyName);
+
+
+/***** 03  Deklaration + Wertzuweisung 2 *******/
+
+// let firstName,familyName;
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachnamen eingeben:");
+// console.log(firstName + " " + familyName);
+
+
+// JS ist eine untypisierte Sprache! | untyped
+
+// let test;
+// test = "Hello";
+// test = 2;
+// test = true;
+
+
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+
+
+/***** 03a  Konstanten *******/
+
+const test = "hallo";
+// test ="hi";
+console.log(test);
