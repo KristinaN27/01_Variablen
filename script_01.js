3a5cfa4493d74ff37f3ffb63e90760f2bf95e85d
Kristina <k.nejgum.kn@gmail.com>
2022-07-08 09:15:50 +0200

    Deklaration + Wertzuweisung

--- script_01.js
+++ script_01.js
@@ -8,9 +8,16 @@
  * 
 */
 
-Konsolen-Ausgabe
-
+// Konsolen-Ausgabe
 // console.log("Hello");
 // console.log(firstName);
 
 /***** 02  Deklaration + Wertzuweisung 1 *******/
+
+let firstName; // Deklaration (Definition)
+firstName = "Max";  // Wertzuweisung
+console.log(firstName);  // Ausgabe
+
+let familyName = "Mütze";  // Deklaration + Wertzuweisung
+console.log(familyName);  // Ausgabe
+console.log(firstName + " " + familyName);
